63d54276e4ad0a08939d3191dd00764e6ea5a3da
minjaaaa <minjadrakul0301@gmail.com>
2025-09-19 11:55:51 +0200

    promenjen model

--- modeling/train_model.py
+++ modeling/train_model.py
@@ -1,9 +1,11 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 def train_model(X_train, y_train): #napravi dodatni fajl, gdje treniras jos modela
     """
     Treniranje RandomForest modela.
     """
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    #model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = KNeighborsClassifier(n_neighbors=5)
     model.fit(X_train, y_train)
     return model
