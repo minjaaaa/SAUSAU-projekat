523b5e511c8389a18ae41ba88dc3b977d1fe7725
minjaaaa <minjadrakul0301@gmail.com>
2025-09-19 18:30:10 +0200

    dodata normalizacija numerickih kolona

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 
 def preprocess_data(X_train, X_test, y_train, y_test):
     """
@@ -14,14 +14,16 @@ def preprocess_data(X_train, X_test, y_train, y_test):
     - Kodiranje ciljne varijable sa LabelEncoder
     """
     
-    # 1. Obrada nepoznatih vrednosti ('?')
+     # 1. Obrada nepoznatih vrednosti ('?')
     X_train.replace('?', 'Unknown', inplace=True)
     X_test.replace('?', 'Unknown', inplace=True)
 
-    # 2. Obrada anomalija u svim numeričkim kolonama
-    num_cols = X_train.select_dtypes(include=np.number).columns.tolist()
+    # 2. Odvajanje numeričkih i kategoričkih kolona
+    cat_cols = X_train.select_dtypes(include=['object', 'category']).columns.tolist()
+    num_cols = X_train.select_dtypes(exclude=['object', 'category']).columns.tolist()
 
-    for col in num_cols: #granice se racunaju samo na trening skupu
+    # 3. Obrada anomalija (outliera) u svim numeričkim kolonama
+    for col in num_cols:
         Q1 = X_train[col].quantile(0.25)
         Q3 = X_train[col].quantile(0.75)
         IQR = Q3 - Q1
@@ -30,25 +32,31 @@ def preprocess_data(X_train, X_test, y_train, y_test):
         upper_bound = Q3 + 1.5 * IQR
         
         X_train[col] = np.clip(X_train[col], lower_bound, upper_bound)
-        X_test[col] = np.clip(X_test[col], lower_bound, upper_bound) #ali granice primenjujem i na test skup
-        #model sada generalizuje, i neka precica tipa age>80 target>=50K
+        X_test[col] = np.clip(X_test[col], lower_bound, upper_bound)
     
-    # 3. Podela kolona na numeričke i kategoričke
-    cat_cols = X_train.select_dtypes(include=['object', 'category']).columns.tolist()
+    # ****************************************************
+    # 4. Normalizacija numeričkih kolona
+    # ****************************************************
+    scaler = StandardScaler() #srednja vr. 0, standardna devijacija 1
+    
+    # Fit na trening skupu i transformacija
+    X_train_num_scaled = scaler.fit_transform(X_train[num_cols])
+    # Samo transformacija na test skupu
+    X_test_num_scaled = scaler.transform(X_test[num_cols]) #ne zelimo da model vidi test skup
 
-    # 4. Kodiranje kategoričkih kolona
+    # 5. Kodiranje kategorijskih kolona
     enc = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
     X_train_cat_encoded = enc.fit_transform(X_train[cat_cols])
     X_test_cat_encoded = enc.transform(X_test[cat_cols])
 
-    # 5. Spajanje enkodiranih kategorija sa numeričkim kolonama
+    # 6. Spajanje enkodiranih kategorija sa normalizovanim numeričkim kolonama
     encoded_feature_names = enc.get_feature_names_out(cat_cols)
     all_feature_names = num_cols + list(encoded_feature_names)
 
-    X_train_encoded = np.hstack([X_train[num_cols], X_train_cat_encoded])
-    X_test_encoded = np.hstack([X_test[num_cols], X_test_cat_encoded])
+    X_train_encoded = np.hstack([X_train_num_scaled, X_train_cat_encoded])
+    X_test_encoded = np.hstack([X_test_num_scaled, X_test_cat_encoded])
 
-    # 6. Kodiranje ciljne varijable
+    # 7. Kodiranje ciljne varijable
     le_target = LabelEncoder()
     y_train_encoded = le_target.fit_transform(y_train)
     y_test_encoded = le_target.transform(y_test)
