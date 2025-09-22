07aef581af4a1b968ca4c19fabcaf85c6e8be8a2
minjaaaa <minjadrakul0301@gmail.com>
2025-09-22 20:36:43 +0200

    dodata matrica korelacije

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from data.load_data import load_adult_data
 from preprocessing.preprocess import preprocess_data
+from preprocessing.visualisation import plot_correlation_matrix
 from modeling.train_model import train_model
 from evaluation.evaluate import evaluate_model
 from utils.helpers import print_separator
+
 
 #ucitavanje podataka iz CSV fajlova
 #uklanjanje duplikata i anomalija
@@ -11,8 +13,10 @@ X_train, X_test, y_train, y_test = load_adult_data(
     train_path="data/adult_train.csv",
     test_path="data/adult_test.csv"
 )
-
+#Generiše i prikazuje matricu korelacije za sve numeričke kolone
+plot_correlation_matrix(X_train)
 #obrada nedostajucih vrednosti i anomalija
+#normalizacija numerickih kolona
 #enkodiranje
 X_train_enc, X_test_enc, y_train_enc, y_test_enc, feature_names = preprocess_data(
     X_train, X_test, y_train, y_test
