1ca6a75d073a52906f97326b3d83d64091a9e713
minjaaaa <minjadrakul0301@gmail.com>
2025-09-23 00:02:29 +0200

    samo matrica konfuzije

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -5,12 +5,12 @@ def evaluate_model(model, X_test, y_test):
     Evaluacija modela: accuracy i confusion matrix.
     """
     y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred, average='weighted')
+    #acc = accuracy_score(y_test, y_pred)
+    #precision = precision_score(y_test, y_pred)
+    #f1 = f1_score(y_test, y_pred, average='weighted')
     cm = confusion_matrix(y_test, y_pred)
-    print(f"Accuracy: {acc:.4f}")
-    print(f"Precision: {precision:.4f}")
-    print(f"F1 score: {f1:.4f}")
+    #print(f"Accuracy: {acc:.4f}")
+    #print(f"Precision: {precision:.4f}")
+    #print(f"F1 score: {f1:.4f}")
     print("Confusion Matrix:")
     print(cm)
