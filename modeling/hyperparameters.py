1b37caf47946d9f50b2e93040b7ffab60759bc05
minjaaaa <minjadrakul0301@gmail.com>
2025-09-23 00:04:18 +0200

    promenjeni grid params

--- modeling/hyperparameters.py
+++ modeling/hyperparameters.py
@@ -3,19 +3,20 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 
 def hyperParam(X_train, y_train):
-    param_grid = {
-        'C': [0.01, 0.1, 1, 10, 100], # Regularizacioni parametar
-        'solver': ['liblinear', 'lbfgs'], # Algoritam za optimizaciju
-        'penalty': ['l1', 'l2'] # Tip regularizacije
-    }
+    param_grid = [
+        # validne kombinacije
+        {'penalty': ['l1'], 'solver': ['liblinear'], 'C': [0.01, 0.1, 1, 10, 100]},
+        
+        {'penalty': ['l2'], 'solver': ['liblinear', 'lbfgs'], 'C': [0.01, 0.1, 1, 10, 100]},
+    ]
 
     logreg = LogisticRegression(random_state=42, max_iter=1000)
     grid_search = GridSearchCV(
         estimator=logreg,
         param_grid=param_grid,
-        cv=5, 
+        cv=3,
         scoring='f1',
-        verbose=1 
+        verbose=1,
     )
     grid_search.fit(X_train, y_train)
     print(f"Najbolji parametri: {grid_search.best_params_}")
