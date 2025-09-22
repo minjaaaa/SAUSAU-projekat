81ba3f9f63147b6cac3916e1c7d5cb1e47a752da
minjaaaa <minjadrakul0301@gmail.com>
2025-09-22 21:11:51 +0200

    dodato jos metrika

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, f1_score
 
 def evaluate_model(model, X_test, y_test):
     """
@@ -6,7 +6,11 @@ def evaluate_model(model, X_test, y_test):
     """
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
     cm = confusion_matrix(y_test, y_pred)
     print(f"Accuracy: {acc:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"F1 score: {f1:.4f}")
     print("Confusion Matrix:")
     print(cm)
