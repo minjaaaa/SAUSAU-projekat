6dde126695c5a27b8529d60cfca03e4e522a056d
minjaaaa <minjadrakul0301@gmail.com>
2025-09-23 15:52:57 +0200

    koristim samo preprocess_data, za obradu

--- main.py
+++ main.py
@@ -9,27 +9,28 @@ from utils.helpers import print_separator
 #ucitavanje podataka iz CSV fajlova
 #uklanjanje duplikata i anomalija
 #podjela na training i test skup
-X_train, X_test, y_train, y_test = load_adult_data(
+""" X_train, X_test, y_train, y_test = load_adult_data(
     train_path="data/adult_train.csv",
     test_path="data/adult_test.csv"
-)
+) """
 #Generiše i prikazuje matricu korelacije za sve numeričke kolone
-plot_correlation_matrix(X_train)
+#plot_correlation_matrix(X_train)
+
 #obrada nedostajucih vrednosti i anomalija
 #normalizacija numerickih kolona
 #enkodiranje
 X_train_enc, X_test_enc, y_train_enc, y_test_enc, feature_names = preprocess_data(
-    X_train, X_test, y_train, y_test
+    train_path="data/adult_train.csv", test_path="data/adult_test.csv"
 )
 
 print_separator()
 print("Podaci su preprocesirani i spremni za treniranje.")
 
 # Treniranje modela i vaznost parametara
-model = choose_and_train_model(X_train_enc, X_test_enc, y_train_enc, y_test_enc, model_type="RandomForest", feature_names=None)
+model = choose_and_train_model(X_train_enc, X_test_enc, y_train_enc, y_test_enc, model_type="LightGBM", feature_names=feature_names)
 print("Model je treniran.")
 
 # Evaluacija
-print_separator()
-#evaluate_model(model, X_test_enc, y_test_enc)
+#print_separator()
+evaluate_model(model, X_test_enc, y_test_enc)
 print_separator()
