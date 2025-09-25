af27aa32d10d9e7e753eb166f00c064a0a908bb5
minjaaaa <minjadrakul0301@gmail.com>
2025-09-25 11:57:01 +0200

    dodati komentari

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 from data.load_data import load_adult_data
 from preprocessing.preprocess import preprocess_data
-from preprocessing.visualisation import plot_correlation_matrix
 from modeling.train_model import choose_and_train_model
 from evaluation.evaluate import evaluate_model, print_most_important_feature
 from utils.helpers import print_separator
 from preprocessing.visualisation import plot_encoded_sex_income_relationship
 from modeling.train_model import print_most_important_feature_per_class_logreg
 
+""" import sys
+print(sys.path) """
 
-#ucitavanje podataka iz CSV fajlova
-#uklanjanje duplikata i anomalija
-#podjela na training i test skup
-""" X_train, X_test, y_train, y_test = load_adult_data(
-    train_path="data/adult_train.csv",
-    test_path="data/adult_test.csv"
-) """
-#Generiše i prikazuje matricu korelacije za sve numeričke kolone
-#plot_correlation_matrix(X_train)
 
-#obrada nedostajucih vrednosti i anomalija
-#normalizacija numerickih kolona
-#enkodiranje
 X_train_enc, X_test_enc, y_train_enc, y_test_enc, feature_names = preprocess_data(
     train_path="data/adult_train.csv", test_path="data/adult_test.csv"
 )
+
 #plot_encoded_sex_income_relationship(X_train_enc, y_train_enc, feature_names)
 print_separator()
 print("Podaci su preprocesirani i spremni za treniranje.")
--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -15,12 +15,10 @@ COLUMN_NAMES = [
 
 def preprocess_data(train_path="data/adult_train.csv", test_path="data/adult_test.csv"):
     """
-    Učitava, čisti i preprocesira podatke za mašinsko učenje.
+    Učitava, čisti i preprocesira podatke 
     
-    Vraća:
-        X_train_encoded, X_test_encoded, y_train_encoded, y_test_encoded, all_feature_names
     """
-    # 1. Učitavanje i početno čišćenje
+    # učitavanje i početno čišćenje
     train_df = pd.read_csv(train_path, header=None, names=COLUMN_NAMES, skipinitialspace=True)
     test_df = pd.read_csv(test_path, header=None, names=COLUMN_NAMES, skipinitialspace=True)
     
@@ -28,57 +26,57 @@ def preprocess_data(train_path="data/adult_train.csv", test_path="data/adult_tes
     #plot_correlation_matrix(train_df)
 
     combined_df = pd.concat([train_df, test_df], ignore_index=True)
-    combined_df.drop_duplicates(inplace=True)
+    combined_df.drop_duplicates(inplace=True) #brisem duplikate
 
     for col in combined_df.select_dtypes(include='object').columns:
-        combined_df[col] = combined_df[col].astype(str).str.strip().str.replace('.', '', regex=False)
-        combined_df[col].replace('?', np.nan, inplace=True)
+        combined_df[col] = combined_df[col].astype(str).str.strip().str.replace('.', '', regex=False) #izbacujem .
+        combined_df[col].replace('?', np.nan, inplace=True) #menjam ? sa NaN
     
-    # Uklanjanje kolone 'fnlwgt' (nije relevantna za predikciju)
+    # uklanjanje kolone 'fnlwgt' (nije relevantna za predikciju)
     combined_df = combined_df.drop('fnlwgt', axis=1)
 
-    # Odvajanje obeležja i ciljne varijable
+    # odvajanje obeležja i ciljne varijable
     X = combined_df.drop('income', axis=1)
     y = combined_df['income']
     
-    # 2. Uklanjanje redova sa NaN vrednostima u ciljnoj varijabli i uklanjanje retkih klasa
+    # uklanjanje redova sa NaN vrednostima u ciljnoj varijabli i uklanjanje retkih klasa
     valid_mask = y.notna()
     X = X[valid_mask].copy()
     y = y[valid_mask].copy()
 
     class_counts = y.value_counts()
-    single_member_classes = class_counts[class_counts < 2].index
+    single_member_classes = class_counts[class_counts < 2].index #nalazim klase koje imaju samo 1 clan
     
     if not single_member_classes.empty:
-        valid_mask_split = ~y.isin(single_member_classes)
+        valid_mask_split = ~y.isin(single_member_classes) #pravim masku redova koje imaju vise od jednog clana
         X = X[valid_mask_split].copy()
         y = y[valid_mask_split].copy()
     
-    # 3. Stratifikovana podela na trening i test set
+    # stratifikovana podela na trening i test set
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, 
         test_size=0.3,
         random_state=42, 
         stratify=y
     )
-    # 4. Odvajanje numeričkih i kategoričkih kolona
+    # odvajanje numeričkih i kategoričkih kolona
     cat_cols = X_train.select_dtypes(include='object').columns.tolist()
     num_cols = X_train.select_dtypes(include=np.number).columns.tolist()
     
-    # 5. Imputacija nedostajućih vrednosti
+    # popunjavanje nedostajućih vrednosti kategoričkih kolona
     for col in cat_cols:
         most_frequent = X_train[col].mode()[0]
-        X_train[col].fillna(most_frequent, inplace=True)
+        X_train[col].fillna(most_frequent, inplace=True) #popunjavam sa klasom koja se najvise puta pojavljuje
         X_test[col].fillna(most_frequent, inplace=True)
-        
+
+    #popunjavanje numerickih kolona  
     for col in num_cols:
-        # Koristimo SimpleImputer za numericke
-        imputer = SimpleImputer(strategy='mean')
+        imputer = SimpleImputer(strategy='mean') #popunjavam sa srednjom vrednosti
         X_train[col] = imputer.fit_transform(X_train[[col]])
         X_test[col] = imputer.transform(X_test[[col]])
 
-    # 6. Obrada outliera u numeričkim kolonama
-    # Isključujemo 'capital-gain' i 'capital-loss' zbog prirode podataka
+    # obrada outliera u numeričkim kolonama
+    # isključujemo capital-gain i capital-loss zbog prirode podataka
     outlier_cols = [col for col in num_cols if col not in ['capital-gain', 'capital-loss']]
     for col in outlier_cols:
         Q1 = X_train[col].quantile(0.25)
@@ -89,23 +87,23 @@ def preprocess_data(train_path="data/adult_train.csv", test_path="data/adult_tes
         X_train[col] = np.clip(X_train[col], lower_bound, upper_bound)
         X_test[col] = np.clip(X_test[col], lower_bound, upper_bound)
 
-    # 7. Normalizacija numeričkih kolona
-    scaler = StandardScaler()
+    # normalizacija numeričkih kolona
+    scaler = StandardScaler() # 0 sa snadradno devijacijom 1, normalizujemo na raspon -1 do 1 
     X_train_num_scaled = scaler.fit_transform(X_train[num_cols])
     X_test_num_scaled = scaler.transform(X_test[num_cols])
 
-    # 8. Kodiranje kategoričkih kolona
-    enc = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+    # kodiranje kategoričkih kolona
+    enc = OneHotEncoder(sparse_output=False, handle_unknown='ignore') #ako naidjes na neku nepozantu vr - ignorisi stavi 0
     X_train_cat_encoded = enc.fit_transform(X_train[cat_cols])
     X_test_cat_encoded = enc.transform(X_test[cat_cols])
 
-    # 9. Spajanje obeležja
+    # spajanje obeležja
     encoded_feature_names = enc.get_feature_names_out(cat_cols)
     all_feature_names = num_cols + list(encoded_feature_names)
     X_train_encoded = np.hstack([X_train_num_scaled, X_train_cat_encoded])
     X_test_encoded = np.hstack([X_test_num_scaled, X_test_cat_encoded])
 
-    # 10. Kodiranje ciljne varijable
+    # kodiranje ciljne varijable
     le_target = LabelEncoder()
     y_train_encoded = le_target.fit_transform(y_train)
     y_test_encoded = le_target.transform(y_test)
